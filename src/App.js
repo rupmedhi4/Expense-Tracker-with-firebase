7035ea8a7cae76a9932825989509736f9ca403c2
Rup medhi <rupmmedhi@gmail.com>
2023-05-19 11:00:57 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Navbar from "./Components/Navbar/Navbar";
 import Signup from "./Components/Signup/Signup";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <ToastContainer/>
-        <Navbar />
-        <Signup />
-        <Login/>
-     
+      <BrowserRouter>
+        <ToastContainer />
+        <Navbar/>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+
+  const loginHandler = () => {
+    navigate("/login")
+  }
+  const signupHandler = () => {
+    navigate("/signup")
+  }
   return (
     <nav className="navbar">
-      <div>
+      <div className='NavDiv'>
         <span>Expense Tracker</span>
-      
-      <ul>
-        <li>
-          <span>Home</span>
-        </li>
-        <li>
-          <span>Products</span>
-        </li>
-        <li>
-          <span>About Us</span>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <span>Home</span>
+          </li>
+          <li>
+            <span>Products</span>
+          </li>
+          <li>
+            <span>About Us</span>
+          </li>
+        </ul>
+      </div>
 
+      <div>
+        <button className="loginButton" onClick={loginHandler}>Login</button>
+        <button className="signupButton" onClick={signupHandler}>Signup</button>
       </div>
     </nav>
   );
